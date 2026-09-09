74da63e1be4cbe0fcc6f4b5c5265377f9faf6271
Jay-Doe <janeksamek@gmail.com>
2026-09-09 22:01:20 +0200

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -3,7 +3,7 @@ class Program
     static void Main()
     {
         while (true) {
-            Console.WriteLine("$ ");
+            Console.Write("$");
             string? x = Console.ReadLine();
             if (x == null){
                 Console.WriteLine("Input is null exiting");
